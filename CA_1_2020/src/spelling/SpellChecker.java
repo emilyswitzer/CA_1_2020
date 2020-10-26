f145e6c3f45528c9796fbb1772045d47fa662152
Emily Switzer <55185998+emilyswitzer@users.noreply.github.com>
2020-10-26 17:05:50 +0000

    Add files via upload

---
+++ CA_1_2020/src/spelling/SpellChecker.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spelling;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+//Use this for profiling spell checking
+public class SpellChecker {
+    private Dictionary dictionary;
+
+    public SpellChecker(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    //This is the method you wish to profile
+    private List<String> doSpellCheck(List<String> words, Dictionary dictionary) {
+        List<String> list = new ArrayList();//list to store mispelt words
+        for (String word : words) {
+            if (!dictionary.isWord(word)) {
+                list.add(word);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hit return to continue");
+
+        //Test with LinkedList dictionary - do separate runs for TreeSet and HashSet dictionaries
+        Dictionary dictionary = new DictionaryLinkedList();
+
+       // Dictionary d2 = new DictionaryHashSet();
+
+       // Dictionary d3 = new DictionaryTreeSet();
+
+        SpellChecker checker = new SpellChecker(dictionary);
+
+        List<String> words = readWords("Alice in Wonderland.txt");
+
+        checker.doSpellCheck(words, dictionary);
+       // checker.doSpellCheck(words, d2);
+       // checker.doSpellCheck(words, d3);
+    }
+
+    public static List<String> readWords(String filename) {
+        List<String> words = new ArrayList();
+        try {
+            Scanner in = new Scanner(new File(filename));
+            // Use any characters other than a-z or A-Z as delimiters
+            in.useDelimiter("[^a-zA-Z]+");
+            while (in.hasNext()) {
+                String s = in.next();
+                words.add(in.next().toLowerCase());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+
+        }
+        return words;
+    }
+}
